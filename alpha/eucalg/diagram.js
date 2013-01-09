3ca0c69b12e9e586a31224bd8530119daf2f9eed
fred6 <frexids@gmail.com>
2013-01-09 00:12:35 -0500

    pass in a canvas object to draw() to support multiple canvases per page

--- alpha/eucalg/diagram.js
+++ alpha/eucalg/diagram.js
@@ -1,7 +1,8 @@
-var c = document.getElementById("c").getContext("2d");
+var c1 = document.getElementById("c1").getContext("2d");
+var c2 = document.getElementById("c2").getContext("2d");
 var PI = Math.PI;
 
-function drawDot ( x, y, r ) {
+function drawDot ( c, x, y, r ) {
     c.beginPath();
     c.arc(x, y, r, 0, 2 * PI);
     c.fill();
@@ -22,7 +23,7 @@ function ngon_coords(N, cx, cy, s, dotrad) {
     return coords;
 }
 
-function draw ( fs, cx, cy, s) {
+function draw ( canvas, fs, cx, cy, s) {
     var N = fs[0];
     var dotrad = 2*s / (N + 3);
     var coords = ngon_coords ( N, cx, cy, s, dotrad );
@@ -31,14 +32,15 @@ function draw ( fs, cx, cy, s) {
     for ( var p = 0; p < coords.length; p++ ) {
         cc = coords[p];
         if ( fs.length > 1 ) {
-            draw ( fs.slice(1), cc[0], cc[1], dotrad );
+            draw ( canvas, fs.slice(1), cc[0], cc[1], dotrad );
         } else {
-            drawDot ( cc[0], cc[1], dotrad );
+            drawDot ( canvas, cc[0], cc[1], dotrad );
         }
     }
 }
 
 
-draw ( [5, 3, 4], 300, 300, 270 );
+draw (c1, [7, 6], 300, 300, 270 );
+draw (c2, [5, 2, 6], 300, 300, 270 );
 
 
