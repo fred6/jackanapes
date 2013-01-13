a8e144e8a138817d397262b98de6f5001de0b4a7
fred6 <frexids@gmail.com>
2013-01-13 12:01:12 -0500

    add subgroup colorings to diagrams

--- eucalg/js/diagram.js
+++ eucalg/js/diagram.js
@@ -2,9 +2,11 @@ var c1 = document.getElementById("c1").getContext("2d");
 var c2 = document.getElementById("c2").getContext("2d");
 var PI = Math.PI;
 
-function drawDot ( c, x, y, r ) {
+function drawDot ( c, x, y, r, color ) {
+    color = color || "#000000";
     c.beginPath();
     c.arc(x, y, r, 0, 2 * PI);
+    c.fillStyle = color;
     c.fill();
 }
         
@@ -23,26 +25,43 @@ function ngon_coords(N, cx, cy, s, dotrad) {
     return coords;
 }
 
-function draw ( canvas, fs, cx, cy, s) {
+// color_config is an object with properties 'number' and 'color'
+//   number -- number of the vertices to color differently
+//   color -- the fill color of the dots
+function draw ( canvas, fs, cx, cy, s, color_config) {
     var N = fs[0];
-    var dotrad = 2*s / (N + 2.5);
+    var dotrad = 2*s / (N + 2.5); // empirically adjusted, no real good reason
     var coords = ngon_coords ( N, cx, cy, s, dotrad );
 
     var cc; //current coord
+    var next_N = fs.slice(1,2);
     for ( var p = 0; p < coords.length; p++ ) {
+
         cc = coords[p];
-        if ( fs.length > 1 ) {
-            draw ( canvas, fs.slice(1), cc[0], cc[1], dotrad );
+        if ( color_config && p < color_config.number ) {
+            if ( fs.length > 1 ) {
+                draw ( canvas, fs.slice(1), cc[0], cc[1], dotrad,
+                        {number: next_N, 
+                         color: color_config.color} );
+            } else {
+                drawDot ( canvas, cc[0], cc[1], dotrad, 
+                          color_config.color );
+            }
         } else {
-            drawDot ( canvas, cc[0], cc[1], dotrad );
+            if ( fs.length > 1 ) {
+                draw ( canvas, fs.slice(1), cc[0], cc[1], dotrad );
+            } else {
+                drawDot ( canvas, cc[0], cc[1], dotrad );
+            }
         }
+
     }
 }
 
 
-//draw (c1, [4, 3], 150, 300, 120 );
-//draw (c1, [3, 4], 450, 315, 120 );
-draw (c1, [7, 2, 3], 300, 300, 270 );
-draw (c2, [5, 4, 3], 300, 300, 270 );
+draw (c1, [5, 3], 150, 300, 115 );
+draw (c1, [7, 3], 450, 315, 115, {number: 5, color: '#d75c1d'} );
+//draw (c1, [7, 2, 3], 300, 300, 270 );
+//kdraw (c2, [5, 4, 3], 300, 300, 270 );
 
 
